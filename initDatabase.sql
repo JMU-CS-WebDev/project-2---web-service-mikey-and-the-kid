29ad89656054bfcd3b290844663480b12eb0c28c
Michael Simonetti <msimonetti.94@gmail.com>
2021-10-29 15:21:32 -0400

    please

--- initDatabase.sql
+++ initDatabase.sql
@@ -3,6 +3,6 @@ DROP USER if EXISTS gym_user@localhost;
 
 CREATE DATABASE gym CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
-CREATE USER gym_user@localhost IDENTIFIED WITH mysql_native_password BY 'rootroot'
+CREATE USER gym_user@localhost IDENTIFIED WITH mysql_native_password BY 'password';
 
-GRANT ALL PRIVILEGES ON gym.* to gym_user@localhost;
+GRANT ALL PRIVILEGES ON gym.* TO gym_user@localhost;
